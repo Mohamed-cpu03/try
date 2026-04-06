2e70662a1352e3929be2c4b2b84059e1875f9e36
mohamed <mohamed.khattabi.it@gmail.com>
2026-04-06 17:35:05 +0100

    Ajout fichiers Java

---
+++ File2.java
@@ -0,0 +1,11 @@
+public class File2 {
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = addition(5, 3);
+        System.out.println("Résultat: " + result);
+    }
+}
---
+++ File3.java
@@ -0,0 +1,10 @@
+public class File3 {
+
+    public static void afficherMessage() {
+        System.out.println("Bonjour depuis File3");
+    }
+
+    public static void main(String[] args) {
+        afficherMessage();
+    }
+}
